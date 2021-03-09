da9a7f4836d705c0c28eae3abed39ed180fe9eb4
“Raheemshah2809” <Raheemshah2809@gmail.com>
2021-03-09 23:13:16 +0000

    second commit, got working a date, countdown timer currently failing.

--- app/src/main/java/com/fluorescentideas/relax/MainActivity.java
+++ app/src/main/java/com/fluorescentideas/relax/MainActivity.java
@@ -3,12 +3,143 @@ package com.fluorescentideas.relax;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView month, day, year;
+    private static final long START_TIME_IN_MILLIS = 60000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+
+        });
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+
+            }
+
+            updateCountDownText();
+        });
+
+
+        private void startTimer () {
+            mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    mTimeLeftInMillis = millisUntilFinished;
+                    updateCountDownText();
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    mTimerRunning = false;
+                    mButtonStartPause.setText("start");
+                    mButtonStartPause.setVisibility(View.INVISIBLE);
+                    mButtonReset.setVisibility(View.VISIBLE);
+                }
+
+
+            }.start();
+
+            mTimerRunning = true;
+            mButtonStartPause.setText("pause");
+            mButtonReset.setVisibility(View.INVISIBLE);
+        }
+
+        private void pauseTimer () {
+            mCountDownTimer.cancel();
+            mTimerRunning = false;
+            mButtonStartPause.setText("Start");
+            mButtonReset.setVisibility(View.INVISIBLE);
+        }
+
+
+        private void resetTimer () {
+            mTimeLeftInMillis = START_TIME_IN_MILLIS;
+            updateCountDownText();
+            mButtonReset.setVisibility(View.INVISIBLE);
+            mButtonStartPause.setVisibility(View.VISIBLE);
+
+        }
+
+    private void updateCountDownText(){
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+
+    }
+
+    }
+
+        month =findViewById(R.id.month);
+        day =findViewById(R.id.day);
+        year =findViewById(R.id.year);
+
+
+
+        Date currentTime = Calendar.getInstance().getTime();
+         String formattedDate =DateFormat.getDateInstance(DateFormat.FULL).format(currentTime);
+
+         String[] splitDate = formattedDate.split(",");
+
+        Log.d("myLOG", formattedDate);
+        Log.d("myLOG", currentTime.toString());
+
+        month.setText(splitDate[1]);
+        day.setText(splitDate[0]);
+        year.setText(splitDate[2]);
+
+
+        Log.d("myLOG", splitDate[0].trim());
+        Log.d("myLOG", splitDate[1].trim());
+        Log.d("myLOG", splitDate[2].trim());
+
     }
 }
